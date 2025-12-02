9522084715fe464e7da8acf06945d64fc0c1d63f
BeverleyRoundTable <beverley247@roundtable.org.uk>
2025-12-02 11:33:29 +0000

    Update routes.js

--- routes.js
+++ routes.js
@@ -10,7 +10,7 @@ function startRoutes() {
     console.log("ROUTE SCRIPT INITIALISED");
 
     const ROUTES_URL =
-    "https://script.google.com/macros/s/AKfycbyvkVzbY9Q59W44PTStHgxbXsvp5PSqZp3IFrhM7PeE4EuY6uuE1obyFEK5tfQp_zGTHg/exec?function=getRoutes";
+    "https://script.google.com/macros/s/AKfycbx0cr7b-3GsKuI02aoITSNDnNNJWJ_HE_IbCm4Iu3PWUrytvMrvwXRYTeKHXaryrEfViw/exec?function=getRoutes";
 
     (async function () {
         try {
